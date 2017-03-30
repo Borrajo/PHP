e8606f5da0c941169436fd669cf672e287eccc3d
micac <micaela.caro@gmail.com>
2017-03-30 16:13:11 -0300

    Modificación de la estructura de las películas

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<!-- Latest compiled and minified CSS -->
+
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
@@ -9,10 +12,231 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<title></title>
+
+<link rel="stylesheet" href="Styles/slide.css" type="text/css" media="screen"> 
+
 </head>
-<body>
-<?php include "/paginas/nav_top.php" ?>
+<body style="background-image:url(http://bsnscb.com/data/out/196/39460826-theatre-wallpapers.jpg)">
+<?php include "paginas/nav_top.php" ?>
 Aca van las peliculitas.
+
+
+<div class="contain">
+  <div class="row">
+    <div class="row__inner">
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="https://k60.kn3.net/taringa/C/C/8/C/4/2/supermachote2/260.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="https://k60.kn3.net/taringa/4/E/9/E/0/6/supermachote2/455.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="hhttp://pics.filmaffinity.com/deadpool-834516798-large.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="http://es.web.img3.acsta.net/pictures/17/01/09/09/56/256507.jpg" alt=""  />
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Nombre
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
 </body>
 </html>
